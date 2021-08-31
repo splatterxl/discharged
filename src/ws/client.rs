8fe87d1dd3e415f49d94d6b27adf85e78f380496
Splatterxl <splatterxl@outlook.ie>
2021-08-31 15:02:50 +0100

    greet user on session token validation

--- src/ws/client.rs
+++ src/ws/client.rs
@@ -2,10 +2,21 @@ use std::net::SocketAddr;
 
 use futures_util::{
 	stream::{SplitSink, SplitStream},
-	StreamExt,
+	SinkExt, StreamExt,
 };
+use serde_json::to_string;
 use tokio::net::TcpStream;
 use tokio_tungstenite::{tungstenite::Message, WebSocketStream};
+
+use crate::ws::schemas::dispatches::Greetings;
+
+use super::{
+	errors::DEFAULT_CLOSE_FRAME,
+	schemas::{
+		dispatches::data_types::PartialUser,
+		gen::{dispatch, greetings},
+	},
+};
 
 pub struct WebSocketClient {
 	pub streams: (
@@ -18,7 +29,7 @@ pub struct WebSocketClient {
 }
 
 impl WebSocketClient {
-	pub fn new(
+	pub async fn new(
 		streams: (
 			SplitSink<WebSocketStream<TcpStream>, Message>,
 			SplitStream<WebSocketStream<TcpStream>>,
@@ -26,8 +37,18 @@ impl WebSocketClient {
 		session_token: String,
 		client_addr: SocketAddr,
 	) -> String {
+		let (mut write, read) = streams;
+
+		if let Err(_) = WebSocketClient::greet(&mut write, &client_addr).await {
+			write
+				.send(Message::Close(Some(DEFAULT_CLOSE_FRAME)))
+				.await
+				.unwrap_or(());
+			return session_token;
+		}
+
 		let client = Self {
-			streams,
+			streams: (write, read),
 			session_token: session_token.clone(),
 			client_addr,
 		};
@@ -35,6 +56,31 @@ impl WebSocketClient {
 		tokio::spawn(WebSocketClient::handle(client));
 
 		session_token
+	}
+
+	pub async fn greet(
+		write: &mut SplitSink<WebSocketStream<TcpStream>, Message>,
+		addr: &SocketAddr,
+	) -> Result<(), ()> {
+		let user = PartialUser {
+			username: String::from("Splatterxl"),
+			nickname: String::from(""),
+			id: 0,
+		};
+
+		println!("{}: greet {} ({})", addr, &user.username, &user.id);
+
+		let res = write
+			.send(Message::Text(
+				to_string(&dispatch::<Greetings>(Some(greetings(user)), 1, None, 1)).unwrap_or(String::from(""))
+			))
+			.await;
+
+        if let Err(_) = res {
+            return Err(());
+        }
+
+		Ok(())
 	}
 
 	pub async fn handle(client: Self) {
