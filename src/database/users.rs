aeb4cb6907cee2d8e4dd830c107948d1ca0503bb
Splatterxl <splatterxl@outlook.ie>
2021-09-05 18:33:30 +0100

    add method for creating users
    Signed-off-by: Splatterxl <splatterxl@outlook.ie>

--- src/database/users.rs
+++ src/database/users.rs
@@ -1,14 +1,37 @@
 use mongodb::bson::doc;
 
-use crate::types::database::User;
+use crate::{types::database::User, util::errors::Errors};
 
 use super::get_collection;
 
-pub fn get(id: String) -> mongodb::error::Result<Option<User>> {
+pub fn get(id: &str) -> mongodb::error::Result<Option<User>> {
 	get_collection::<User>("users").find_one(
 		doc! {
-		  "_id": id
+		  "_id": String::from(id)
 		},
 		None,
 	)
+}
+
+pub fn create(data: User) -> Result<(), Errors> {
+	match get(&data.id) {
+		Ok(user) => {
+			if user.is_some() {
+				Err(Errors::UserAlreadyExists)
+			} else {
+				if let Err(err) = get_collection::<User>("users").insert_one(&data, None) {
+					return Err(Errors::UnknownError {
+						action: String::from("create user"),
+						error: Box::new(err),
+					});
+				}
+				println!("Created user {} (id {})", data.username, data.id);
+				Ok(())
+			}
+		}
+		Err(err) => Err(Errors::UnknownError {
+			action: String::from("create"),
+			error: Box::new(err),
+		}),
+	}
 }
--- src/types/database.rs
+++ src/types/database.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, Deserialize, Serialize)]
 pub struct User {
 	#[serde(rename = "_id")]
-	id: String,
-	username: String,
-	nickname: String,
+	pub id: String,
+	pub username: String,
+	pub nickname: Option<String>,
 }
